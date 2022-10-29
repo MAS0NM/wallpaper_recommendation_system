ae3d7e6bb9bf660df4caca8e05fbf37b05f4e04b
mark-HFUT <1546662938@qq.com>
2022-10-29 21:35:10 +0800

    'v2'

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
 import os
-from recDataLoader import dataLoader
-from similarity_matrix import similarity_matrix
-from recom import itemCF
+from .recDataLoader import dataLoader
+from .sim_matrix import similarity_matrix
+from .recom import itemCF
 import pickle
-
+root_dir = os.path.dirname(os.path.realpath(__file__))
+print (root_dir)
 test_input = {
     'table':{
         'user1':{
@@ -30,40 +31,35 @@ def getUserRating(user, table):
             rating_dict[item] = 0
     return rating_dict
 
-def rec_init(objects_path, filelist_path, img_dir, featureMap_path):
+def rec_init(objects_path='./objects', filelist_path='./filelist/filelist.csv', img_dir='./imgs', featureMap_path='./features/features.json'):
     dl, sm, cf = None, None, None
     try:
-        with open(os.path.join(objects_path, 'dl.pkl'), 'rb') as f:
+        with open(os.path.join(root_dir,objects_path, 'dl.pkl'), 'rb') as f:
             print('loading dl')
             dl = pickle.load(f)
     except:
+            print('dumping dl')
             dl = dataLoader(filelist_path, img_dir, featureMap_path)
             with open(os.path.join(objects_path, 'dl.pkl'), 'wb') as f:
-                print('dumping dl')
                 pickle.dump(dl, f)
             
     try:
-        with open(os.path.join(objects_path, 'sm.pkl'), 'rb') as f:
+        with open(os.path.join(root_dir,objects_path, 'sm.pkl'), 'rb') as f:
             print('loading sm')
             sm = pickle.load(f)
     except:
+        print('dumping sm')
         sm = similarity_matrix(dl)
         with open(os.path.join(objects_path, 'sm.pkl'), 'wb') as f:
-            print('dumping sm')
             pickle.dump(sm, f)
             
     try:
-        with open(os.path.join(objects_path, 'cf.pkl'), 'rb') as f:
+        with open(os.path.join(root_dir,objects_path, 'cf.pkl'), 'rb') as f:
             print('loading cf')
             cf = pickle.load(f)
     except:
+        print('dumping cf')
         cf = itemCF(dl, sm)
-        with open(os.path.join(objects_path, 'cf.pkl'), 'wb') as f:
-            print('dumping cf')
+        with open(os.path.join(root_dir,objects_path, 'cf.pkl'), 'wb') as f:
             pickle.dump(cf, f)
     return cf
-
-rating_dict = getUserRating('user1', test_input['table'])
-cf = rec_init('./objects', './filelist/filelist.csv', './imgs', './features/features.json')
-output = cf.recommend(top=20, rating_dict=rating_dict, shuffle=True)
-print(output)
--- recom.py
+++ recom.py
@@ -1,15 +1,16 @@
 import numpy as np
 from numpy.core.getlimits import inf
 import random
+import math
 class itemCF:
-    def __init__(self, dl, sm, ratingDict):
+    def __init__(self, dl, sm):
         self.img2idx = dl.item2index
         self.idx2img = dl.items_list
         self.img2lab = dl.filelist
         self.sim_mat = sm.collaborative_similarity_matrix
     
     def getRatingRank(self, rating_dict):
-        ratingRank = sorted(list(self.ratingDict.items()), key=lambda x:x[1], reverse=True)
+        ratingRank = sorted(list(rating_dict.items()), key=lambda x:x[1], reverse=True)
         return ratingRank
         
     def recommend(self, top, rating_dict, shuffle=False):
@@ -21,19 +22,22 @@ class itemCF:
         rec_rank_with_rate = []
         rec_rank = []
         
-        for item in self.ratingRank:
-            rec_item_num_dict[item[0]] = int(item[1] / total_rate * top)
+        for item in ratingRank:
+            rec_item_num_dict[item[0]] = math.ceil(int(item[1] / total_rate * top))
             
-        for item in self.ratingRank:
+        for item in ratingRank:
             img_name = item[0]
             img_rate = item[1]
             max_rate = max(img_rate, max_rate)
             min_rate = min(img_rate, min_rate)
             idx = self.img2idx[img_name]
             topn = rec_item_num_dict[img_name]
-            idx_rank = np.argsort(-self.sim_mat[idx])[:topn]
+            idx_rank = np.argsort(-self.sim_mat[idx])[:topn+1]
             img_rank = [(self.idx2img[i], img_rate) for i in idx_rank]
             for img_rate in img_rank:
+                for i in rec_rank_with_rate:
+                    if i[0] == img_rate[0]:
+                        continue
                 rec_rank_with_rate.append(img_rate)
         
         if shuffle:
@@ -53,4 +57,4 @@ class itemCF:
                 score -= 1
         else:
             rec_rank = [i[0] for i in rec_rank_with_rate]
-        return rec_rank
+        return rec_rank[:top]
